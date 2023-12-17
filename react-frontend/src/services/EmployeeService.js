6048ba4c2d0323c5bee1114f13c39a442406c7a6
selvanayaki678 <selvanayaki849@gmail.com>
2023-12-17 20:49:28 +0530

    Created the jenkins file

---
+++ react-frontend/src/services/EmployeeService.js
@@ -0,0 +1,28 @@
+import axios from 'axios';
+
+const EMPLOYEE_API_BASE_URL = "http://a1382658c6dd04164bd5aff3de8a0349-1472821101.us-east-2.elb.amazonaws.com:8081/api/v1/employees";
+
+class EmployeeService {
+
+    getEmployees(){
+        return axios.get(EMPLOYEE_API_BASE_URL);
+    }
+
+    createEmployee(employee){
+        return axios.post(EMPLOYEE_API_BASE_URL, employee);
+    }
+
+    getEmployeeById(employeeId){
+        return axios.get(EMPLOYEE_API_BASE_URL + '/' + employeeId);
+    }
+
+    updateEmployee(employee, employeeId){
+        return axios.put(EMPLOYEE_API_BASE_URL + '/' + employeeId, employee);
+    }
+
+    deleteEmployee(employeeId){
+        return axios.delete(EMPLOYEE_API_BASE_URL + '/' + employeeId);
+    }
+}
+
+export default new EmployeeService() 
